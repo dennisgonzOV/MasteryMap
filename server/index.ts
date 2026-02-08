1aafe5e4a9a2b36f0a2026f3efd28b17fd84a05e
dennisgonzOV <dennisgonz13@gmail.com>
2026-02-07 21:14:40 -0500

    feat: Allow server port to be configured via environment variable with a 5000 fallback.

--- server/index.ts
+++ server/index.ts
@@ -88,10 +88,8 @@ handleUncaughtExceptions();
     // Global error handler
     app.use(errorHandler);
 
-    // ALWAYS serve the app on port 5000
-    // this serves both the API and the client.
-    // It is the only port that is not firewalled.
-    const port = 5000;
+    // Use PORT from environment or fallback to 5000
+    const port = parseInt(process.env.PORT || "5000");
     server.listen({
       port,
       host: "0.0.0.0",
